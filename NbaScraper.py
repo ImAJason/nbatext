5563d3b300916581793b225ba97ec3e5df043686
imajason <jjaswang@gmail.com>
2018-06-06 20:58:16 -0500

    quote

--- NbaScraper.py
+++ NbaScraper.py
@@ -10,7 +10,7 @@ class ScrapeNba:
 
     def get_data(self, date):
 
-        link = "http://scores.nbcsports.com/nba/scoreboard.asp?day="+date+"&meta=true
+        link = "http://scores.nbcsports.com/nba/scoreboard.asp?day="+date+"&meta=true"
 
         response = requests.get(link)
         sourceCode = response.content
